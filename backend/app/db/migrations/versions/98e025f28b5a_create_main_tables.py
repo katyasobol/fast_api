d2e3b9188a870d4ab9c7b86431d77a52f187ff78
Ekaterina <katyshik2000@yandex.com>
2023-02-23 00:17:20 +0300

    commit

---
+++ backend/app/db/migrations/versions/98e025f28b5a_create_main_tables.py
@@ -0,0 +1,33 @@
+"""create_main_tables
+Revision ID: 98e025f28b5a
+Revises: 
+Create Date: 2023-02-22 21:09:14.214748
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic
+revision = '98e025f28b5a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def create_cleanings_table() -> None:
+    op.create_table(
+        "cleanings",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("name", sa.Text, nullable=False, index=True),
+        sa.Column("description", sa.Text, nullable=True),
+        sa.Column("cleaning_type", sa.Text, nullable=False,
+                  server_default="spot_clean"),
+        sa.Column("price", sa.Numeric(10, 2), nullable=False),
+    )
+
+
+def upgrade() -> None:
+    create_cleanings_table()
+
+
+def downgrade() -> None:
+    op.drop_table("cleanings")
